f14e1ebe0977fd9f579fb0bef0307a2a8a2becca
Kaviyarasansoftdev <kaviyarasan.tcz@gmail.com>
2025-06-19 13:08:39 +0530

    endpoint changes

--- src/shared/services/apiendpoint/apiendpoint.js
+++ src/shared/services/apiendpoint/apiendpoint.js
@@ -1,5 +1,6 @@
 const apiurl=()=>{
-    var url="http://192.168.29.44:4000";
+    // var url="http://192.168.29.44:4000";
+    var url = "https://crm-backend-demo.vercel.app"
     // var url="http://52.65.221.18:5000";
     return url;
 }
